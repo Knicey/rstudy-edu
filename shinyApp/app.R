777649f892ee85c99c92616636145bf8aaafe2a2
Nathan Yang <nathan.s.yang@gmail.com>
2024-04-19 09:04:54 -0400

    More data fixes and customizations

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -7,9 +7,17 @@ library(scales)
 library(bslib)
 library(bsicons)
 library(rsconnect)
+library(dplyr)
 
 # Load data --------------------------------------------------------------------
 
+#Helper Functions -------------------------------------------------------------
+
+convert_percentage <- function(x){
+  as.numeric(sub("%", "", x, fixed=TRUE))/100
+}
+
+#Raw Dataset For Academic Performance -----------------------------------------
 test_admindist_gys <- read_csv('data/seda2023_admindist_poolsub_gys_updated_20240205.csv')
 
 test_selected_years <- test_admindist_gys |>
@@ -34,17 +42,30 @@ test_wider <- test_selected_years |>
     values_from = c(gys_mn_2019_ol, gys_mn_2022_ol, gys_mn_2023_ol)
   )
 
-acs_income_2021 <- read_csv('data/acs_income_2021.csv')
+#Cleaned Dataset for Income ----------------------------------------------------
+acs_income_2021 <- read_csv('data/acs_income_2021.csv') 
 
-acs_income_2021_joined <- acs_income_2021 |>
+acs_income_2021_narrow <- acs_income_2021 |>
+  rename(
+    ssi_percent = household_income_all_households_with_social_security_income,
+    wages_salary = household_income_all_households_with_earnings_with_wages_or_salary_income,
+    public_assistance = household_income_all_households_with_cash_public_assistance_income_or_food_stamps_snap,
+    retirement = household_income_all_households_with_retirement_income,
+  ) |>
+  select_if(~ (is.character(.x))) |>
+  relocate(state)
+
+#Turn all percentages to numeric ---------------------------------------------
+acs_income_2021_narrow[,-(1:2)] <- apply(acs_income_2021_narrow[, -(1:2)],2, function(x){
+  as.numeric(sub("%", "", x, fixed=TRUE))/100})
+
+#Join income and test data ---------------------------------------------------
+acs_income_2021_joined <- acs_income_2021_narrow |>
   left_join(
     test_longer,
     by = join_by(school_district == sedaadminname, state == stateabb)
-  ) |>
-  rename(
-    ssi_percent = household_income_all_households_with_social_security_income,
-    wages_salary = household_income_all_households_with_earnings_with_wages_or_salary_income
   )
+
 
 # Find all choices ----------------------------------------------------------
 
@@ -58,6 +79,14 @@ subject_choices <- test_selected_years |>
   arrange(subject) |>
   pull(subject)
 
+income_choices <- acs_income_2021_narrow |>
+  select(
+    ssi_percent,
+    wages_salary,
+    public_assistance,
+    retirement
+  )  |>
+  colnames()
 
 #Take out year 2019 (this is the basis of comparison) ------------------------
 year_choices <- test_longer |>
@@ -73,6 +102,8 @@ selected_subgroup <- "all"
 selected_subject <- sample(subject_choices, 1)
 
 selected_year <- "2022"
+
+selected_income <- sample(income_choices, 1)
 
 # Define UI --------------------------------------------------------------------
 
@@ -104,6 +135,14 @@ ui <- page_navbar(
       selected = selected_subject,
       options = list(plugins = "remove_button")
     ),
+    selectizeInput(
+      inputId = "income",
+      label = "Select an economic variable:",
+      multiple = FALSE,
+      choices = income_choices,
+      selected = selected_income,
+      options = list(plugins = "remove_button")
+    ),
     
     "Data comes from the Educational Opportunity Project at Stanford University."
   ),
@@ -117,15 +156,15 @@ ui <- page_navbar(
   ),
   
   nav_panel(
-    title = "Academic Peformance by Subgroup",
+    title = "Academic Peformance by Economic Variable",
     card(
-      card_body(plotOutput(outputId = "ssi_vs_math_plot"))
+      card_body(plotOutput(outputId = "ecnomic_vs_score_plot"))
     )
   ),
 
   nav_panel(
     title = "Data",
-    card(card_body(DT::dataTableOutput(outputId = "data"))),
+    card(card_body(DT::dataTableOutput(outputId = "data")))
   ),
   footer = "Showing only results for school districts in the US."
   
@@ -155,8 +194,14 @@ server <- function(input, output, session) {
     test_wider_filtered() |>
       ggplot(aes(x = gys_mn_2023_ol_mth, y = gys_mn_2023_ol_rla)) +
       geom_point(aes(color = gys_mn_2023_ol_rla)) +
-      scale_color_gradientn(colours = c('white', "skyblue", 'red'),
-                            values = c(0, -1, 1)) +
+      #scale_color_gradientn(
+      #  colors = c('white', "skyblue", 'red'),
+      #  values = c(0, -1, 1)
+      #) +
+      scale_color_gradientn(
+        colors = c("red", "#FFFFE4", "skyblue"), 
+        limits = c(-7, 7)
+      ) +
       geom_smooth(method = "lm", se = FALSE) +
       labs(
         x = "Math Scores",
@@ -173,23 +218,24 @@ server <- function(input, output, session) {
       scale_x_continuous(limits = c(-7, 7) )
   })
   
-  # Plot of test scores vs ssi percentage
-  output$ssi_vs_math_plot <- renderPlot({
+  # Plot of test scores vs economic variable
+  output$ecnomic_vs_score_plot <- renderPlot({
     acs_income_joined_filtered() |>
-      ggplot(aes(x = ssi_percent, y = score)) +
+      ggplot(aes(x = get(input$income), y = score)) +
       geom_point(aes(color = score)) +
-      scale_color_gradientn(colours = c('white', "skyblue", 'red'),
+      scale_color_gradientn(colors = c('white', "skyblue", 'red'),
                             values = c(0, -1, 1)) +
       geom_smooth(method = "lm", se = FALSE) +
       labs(
-        x = "Percentage of Families Receiving SSI",
+        x = paste("Percentage of Families", input$income),
         y = paste(input$subject , "Scores"),
         title = paste(
           "Change in", 
           input$subject,
           "Performance from 2019 to", 
           input$year, 
-          "vs Percentage of Families Receiving SSI"
+          "vs",
+          input$income
           ),
         subtitle = paste("Among", input$subgroup, "students")
       ) +
@@ -204,12 +250,7 @@ server <- function(input, output, session) {
 
   
   output$data <- DT::renderDataTable({
-    test_wider_filtered() |>
-      select(
-        sedaadminname, stateabb, 
-        gys_mn_2019_ol_mth, gys_mn_2019_ol_rla, 
-        gys_mn_2022_ol_mth, gys_mn_2022_ol_rla,
-        gys_mn_2023_ol_mth, gys_mn_2023_ol_rla)
+    acs_income_joined_filtered() 
   })
 }
 
