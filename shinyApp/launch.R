fb4d0b34b7d496a9504ba58cfcbbc401cb21080b
Nathan Yang <nathan.s.yang@gmail.com>
2024-05-02 12:27:09 -0400

    Deleted launch file

--- shinyApp/launch.R
+++
@@ -1,5 +0,0 @@
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='nathany', token='505C6D6E45CB82B211B6418446055DA5', secret='Kbgiq4XHaqQeFCChUGVBgQLooaIK8HAFveDqcj5T')
-
-rsconnect::deployApp("shinyApp", appName = "education-statistics")
